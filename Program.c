49a01356d2632eaae5068e5943ad93fa56729fc7
Lakshan <49695134+Lakshan77@users.noreply.github.com>
2020-10-20 12:22:38 +0530

    Update Program.c
    Added some variables and did some changes in the code.

--- Program.c
+++ Program.c
@@ -2,17 +2,21 @@
 
 int main()
 {
-    char name,country,skill;
+    char name[50];
+    char country[50];
+    char skill;
+    int age;
     printf("Welcome to Hacktoberfest 2020\n");
-    printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
+
+    printf("Enter your name :");//Get User name
+    scanf("%s",&name);
     printf("Enter your Country of Residance : ");//Get user country
     scanf("%s",&country);
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
-    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
-    scanf(" %c",&skill);
-    scanf("Thank you %c Happy Hacking",country);//Thank the user
+    scanf("%d",&age);
+    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest :\n");//Get user skill level
+    scanf("%c",&skill);
+    printf("Thank you Happy Hacking %s", name);//Thank the user
 
     return 0;
 }
